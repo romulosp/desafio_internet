c95ff8d96025ce403b0ea79ff0bbafe997a9ed1c
romulosp <romulos.p@hotmail.com>
2019-04-20 16:28:27 -0300

    criacao do projeto desafio internet 1

---
+++ src/main/java/com/br/romulo/desafio_internet/DesafioInternetApplication.java
@@ -0,0 +1,13 @@
+package com.br.romulo.desafio_internet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInternetApplication.class, args);
+	}
+
+}
